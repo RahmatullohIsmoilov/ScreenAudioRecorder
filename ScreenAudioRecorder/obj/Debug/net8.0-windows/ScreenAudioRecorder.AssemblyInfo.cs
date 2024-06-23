aad132b7ff780970a6432ff62381113d2e7f8793
Rahmatulloh <161015863+RahmatullohIsmoilov@users.noreply.github.com>
2024-06-23 23:29:30 +0500

    A Completed Version

--- ScreenAudioRecorder/obj/Debug/net8.0-windows/ScreenAudioRecorder.AssemblyInfo.cs
+++ ScreenAudioRecorder/obj/Debug/net8.0-windows/ScreenAudioRecorder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenAudioRecorder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fa457e22c6235cc6ca24b51a4feb277dbb36d46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b633ccb51c1c610b7bc1189645fc369656b19e94")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenAudioRecorder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenAudioRecorder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
